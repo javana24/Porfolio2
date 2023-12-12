3f430e323058a48544f8e921100529b0ecffe8d1
javana24 <145677760+javana24@users.noreply.github.com>
2023-12-12 13:01:37 +0100

    Add files via upload

--- Proyecto/javascript/funciones.js
+++ Proyecto/javascript/funciones.js
@@ -1,11 +1,13 @@
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
 
 function toggleReadMore() {
     var extraContent = document.querySelector('.extra-content');
